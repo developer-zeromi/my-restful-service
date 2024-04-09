e61e0b5d2bc205f9ee1dcea516317ebfb2c6e9f7
youngmi <devzeromi@gmail.com>
2024-04-09 23:19:58 +0900

    Entity 설정과 Spring Data JPA를 이용한 초기 데이터 생성

--- src/main/java/kr/co/kimyoungmi/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/kr/co/kimyoungmi/myrestfulservice/config/SecurityConfig.java
@@ -1,32 +1,40 @@
 package kr.co.kimyoungmi.myrestfulservice.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//
-//        UserDetails newUser = User.withUsername("user")
-//                .password(passwordEncoder().encode("passw0rd"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsManager.createUser(newUser);
-//        return userDetailsManager;
-//    }
-//
-//    // 인코딩 할 수 있는 알고리즘 객체
-//    @Bean
-//    BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("passw0rd"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    // 인코딩 할 수 있는 알고리즘 객체
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean // 인증 무시하고자 하는 옵션 지정
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values (90001, now(), 'User1', 'test111', '701010-1111111');
+insert into users(id, join_date, name, password, ssn) values (90002, now(), 'User1', 'test222', '801010-1111111');
+insert into users(id, join_date, name, password, ssn) values (90003, now(), 'User2', 'test333', '901010-1111111');

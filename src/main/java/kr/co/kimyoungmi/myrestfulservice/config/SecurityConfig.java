eaa0032751493a3ffba98e8738d784efacbe7ff6
youngmi <devzeromi@gmail.com>
2024-04-09 23:03:33 +0900

    JPA 사용을 위한 Dependency 추가와 초기 설정

--- src/main/java/kr/co/kimyoungmi/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/kr/co/kimyoungmi/myrestfulservice/config/SecurityConfig.java
@@ -1,32 +1,32 @@
 package kr.co.kimyoungmi.myrestfulservice.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
+//@Configuration
 public class SecurityConfig {
-    @Bean
-    UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-
-        UserDetails newUser = User.withUsername("user")
-                .password(passwordEncoder().encode("passw0rd"))
-                .authorities("read")
-                .build();
-
-        userDetailsManager.createUser(newUser);
-        return userDetailsManager;
-    }
-
-    // 인코딩 할 수 있는 알고리즘 객체
-    @Bean
-    BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+//
+//        UserDetails newUser = User.withUsername("user")
+//                .password(passwordEncoder().encode("passw0rd"))
+//                .authorities("read")
+//                .build();
+//
+//        userDetailsManager.createUser(newUser);
+//        return userDetailsManager;
+//    }
+//
+//    // 인코딩 할 수 있는 알고리즘 객체
+//    @Bean
+//    BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
